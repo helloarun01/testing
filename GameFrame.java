61623e36f2cc92df1a07687ec49e43503b19542b
helloarun01 <168637891+helloarun01@users.noreply.github.com>
2025-05-20 01:29:47 +0600

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    private final int tileSize = 32;
+    private final int rows = 21;
+    private final int cols = 19;
+
+    public GameFrame(){
+        setTitle("Pac-Man");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        GamePanel panel = new GamePanel(rows, cols, tileSize);
+        add(panel);
+        pack();
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        panel.requestFocusInWindow();
+        
+    }
+
+    public void start(){}
+    
+}
